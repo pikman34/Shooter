3c48b9252e545d8082a850220a31b0a9bd43d63e
Noah <noahbob34@gmail.com>
2025-04-06 16:18:34 +0100

    created bullet code, need to update once i get code for platforms created

--- regrowth/bullet.gd
+++ regrowth/bullet.gd
@@ -1,1 +1,22 @@
 extends Area2D
+
+@export var speed = 200
+@export var damage = 15
+
+var direction:Vector2
+
+func set_direction(bulletDirection):
+	direction = bulletDirection
+	rotation_degrees = rad_to_deg(global_position.angle_to_point(global_position+direction))
+	
+	
+func _physics_process(delta):
+	global_position += direction * speed * delta
+
+
+func _on_body_entered(body):
+	if body.is_in_group("enemies"):
+		body.get_damage(damage)
+		queue_free()
+	if body.is_in_group("platforms"):
+		queue_free()
