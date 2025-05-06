257b0743e4d610277f6f803c17d4673bf2636a66
pikman34 <noahbob34@gmail.com>
2025-05-06 23:46:09 +0100

    added moving platforms

--- regrowth/Scripts/game.gd
+++ regrowth/Scripts/game.gd
@@ -3,3 +3,9 @@ extends Node2D
 @export var world_speed = 300
 
 @onready var moving_environment = $Environment/Moving
+
+var platform = preload("res://Scenes/platform.tscn")
+var rng = RandomNumberGenerator.new()
+
+func _physics_process(delta):
+	moving_environment.position.x -= world_speed * delta
